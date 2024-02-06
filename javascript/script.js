24efa8e5d7ec965624f2779d1038d7cd00f1810f
Kayky <114265239+kaykyrod@users.noreply.github.com>
2024-02-05 21:47:55 -0300

    alterações no style e criação das funções

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,29 @@
-/*
-
-function limparCampo() {
-    document.querySelector('.campo_resultado_geral').style.display = 'none';
-}
+let resultadoCriptografado = '';
 
 function criptografar() {
-    limparCampo();
+    let texto = document.querySelector('#campo_texto').value;
+    resultadoCriptografado = texto.replaceAll('e', 'enter')
+                                  .replaceAll('i', 'imes')
+                                  .replaceAll('a', 'ai')
+                                  .replaceAll('o', 'ober')
+                                  .replaceAll('u', 'ufat');
 
-    includes
-    replace 
+    document.querySelector('.campo_resultado').style.textAlign = 'start';
+    document.querySelector('.campo_resultado').innerHTML = resultadoCriptografado;
+
+    console.log(resultadoCriptografado);
 }
-*/
+
+function descriptografar() {
+    let resultadoDescriptografado = resultadoCriptografado.replaceAll('enter', 'e')
+                                                          .replaceAll('imes', 'i')
+                                                          .replaceAll('ai', 'a')
+                                                          .replaceAll('ober', 'o')
+                                                          .replaceAll('ufat', 'u');
+
+    document.querySelector('.campo_resultado').innerHTML = resultadoDescriptografado;
+
+    console.log(resultadoDescriptografado);
+}
+
+
